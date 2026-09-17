699077683407b4a0d5bde26703aa5cd682349825
Rakib Hussain <156150712+iamrakibhussain@users.noreply.github.com>
2026-09-18 02:09:29 +0600

    maked correction

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <section>
       <h1 className="text-center">
-        The Hub for Every Dev <br /> Event You Can't Miss
+        The Hub for Every Dev <br /> Event You Can&apost Miss
       </h1>
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
